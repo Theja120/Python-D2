100b265886a9d7c8ef6c561a718ae26d816df644
Theja120 <ravitheja7098@gmail.com>
2025-04-05 18:45:51 +0530

    Day_2

---
+++ Day_2.py
@@ -0,0 +1,26 @@
+# list , Tupple, Range, String- Sequences
+# Indexing, Slicing, mutable, immutable
+
+#String - and its operations
+# Sequence of charecters is called a String
+str = "Ravi Theja"
+print(len(str))
+print(str[5])
+print(str[0:5])
+# str(0)="t"
+# print(str)
+
+#List - Collection of heterogenous elements 
+list = [1,2,3,4.0,5.0,6.0,"string",1+2j,True]
+print(len(list))
+print(list[6])
+print(list[0:6])
+list[3] = "Move"
+print(list)
+
+#tupple 
+tupple = (1,2,3,4,5)
+print(len(tupple))
+print(tupple[0])
+print(tupple[:])
+
